5194d0d526f6cd283f7b3f0b1a6c2768bb21a5c4
Joe Donino <joedonino@gmail.com>
2026-02-19 14:50:49 -0500

    Initial Commit

---
+++ DogDetectorTests/DogDetectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  DogDetectorTests.swift
+//  DogDetectorTests
+//
+//  Created by Joe Donino on 2/19/26.
+//
+
+import Testing
+@testable import DogDetector
+
+struct DogDetectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
